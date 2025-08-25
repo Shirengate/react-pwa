ccc61435e820681d8b77f5a068722c7d2a2a017a
Shirengate <sweetgames551ggg@gmail.com>
2025-08-25 18:27:47 +0300

    add persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./assets/index.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { persister, store } from "./store/store";
 
 import routes from "./router/router";
 import { RouterProvider } from "react-router";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <RouterProvider router={routes} />
+    <PersistGate persistor={persister}>
+      <RouterProvider router={routes} />
+    </PersistGate>
   </Provider>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,15 +3,41 @@ import { useDispatch, useSelector } from "react-redux";
 import { taskApi } from "./api/tasks";
 import { postApi } from "./api/posts";
 import { favoritePosts } from "./reducer/favorite";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "favorites",
+  storage,
+};
+
+const persistedFavoriteReducer = persistReducer(
+  persistConfig,
+  favoritePosts.reducer
+);
 export const store = configureStore({
   reducer: {
     [taskApi.reducerPath]: taskApi.reducer,
     [postApi.reducerPath]: postApi.reducer,
-    favoritePosts: favoritePosts.reducer,
+    favoritePosts: persistedFavoriteReducer,
   },
   middleware: (fn) => {
-    return fn().concat(taskApi.middleware).concat(postApi.middleware);
+    return fn({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+      .concat(taskApi.middleware)
+      .concat(postApi.middleware);
   },
 });
 
@@ -21,3 +47,5 @@ export type AppDispatch = typeof store.dispatch;
 export const useAppSelector = useSelector.withTypes<RootState>();
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
+export const persister = persistStore(store);
